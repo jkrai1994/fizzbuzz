d5546b324835dc62a91d281cdc4c535d0f3a4af9
jitendra <jkrai1994@gmail.com>
2024-01-16 17:25:20 +0530

    removed comment

--- src/main/java/com/hackathon/fizzbuzz/FizzbuzzApplication.java
+++ src/main/java/com/hackathon/fizzbuzz/FizzbuzzApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.hackathon.fizzbuzz"})
 public class FizzbuzzApplication {
-	//test
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(FizzbuzzApplication.class, args);
